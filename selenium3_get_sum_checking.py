2251040a464d6115288631cc053480034ceb217e
kefarova-png <kefarova@gmail.com>
2026-06-23 23:48:19 +0400

    Version 3. Исправлены/доработаны except'ы. Добавлена информативность в консоли

--- selenium3_get_sum_checking.py
+++ selenium3_get_sum_checking.py
@@ -4,6 +4,23 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
+
+
+def press_allow_all():  #  Находим и жмём всплывшее [Allow all], чтобы не мешало(сь)
+    wait = WebDriverWait(driver, 3)
+    try:
+        allow_all_button = wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//button[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']")
+            )
+        )
+        allow_all_button.click()
+        print('Кнопка [Allow all] нажата')
+    except TimeoutException:
+        print('Кнопка [Allow all] не отобразилась')
 
 
 #  Chrome. Создаём переменную для опций браузера
@@ -18,46 +35,49 @@ driver = webdriver. Chrome (
 base_url = 'https://lambdatest.com/selenium-playground/simple-form-demo'
 driver.get(base_url)
 driver.set_window_size(920, 1080)
-time.sleep(5)
+
+#  Находим и жмём всплывшее [Allow all], чтобы не мешало(сь)
+press_allow_all()
 
 #  Найдем поле ввода текста и введем текст
 text_field = driver.find_element(By.XPATH, "//*[@id='user-message']")
 text_value = 'A text for a test'
 text_field.send_keys(text_value)
-time.sleep(5)
+print('Текстовое поле заполнено')
+time.sleep(1)
 
 #  Найдем поля ввода слагаемых и введём в каждое по одному числу
 first_field = driver.find_element(By.XPATH, "//input[@id='sum1']")
 second_field = driver.find_element(By.XPATH, "//input[@id='sum2']")
-first_value = 100
-second_value = 200
+first_value = 0.1
+second_value = 0.9
 first_field.send_keys(first_value)
 second_field.send_keys(second_value)
-time.sleep(5)
+print('Числовые поля заполнены')
+time.sleep(1)
 
 #  Находим и жмём всплывшее [Allow all], чтобы не мешало(сь)
-try:
-    driver.find_element(
-        By.XPATH,
-        "//button[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']"
-    ).click()
-except:
-    pass
-time.sleep(2)
+press_allow_all()
 
 #  Находим и жмём [Get Checked Value]
 driver.find_element(By.XPATH,"//button[@id='showInput']").click()
-time.sleep(0.1)
+print('Кнопка [Get Checked Value] нажата')
+time.sleep(1)
 
 #  Находим и жмём [Get Sum]
 driver.find_element(By.XPATH,"//*[@id='gettotal']/button").click()
+print('Кнопка [Get Sum] нажата')
+time.sleep(1)
 
 #  Проверяем соответствие отображаемого текста ожидаемому
 fact_text = None  #  если отображаемый в превью текст не найдется
 try:
-    fact_text = driver.find_element(By.XPATH,"//p[@id='message']").text
-except:
-    pass
+    text_element = driver.find_element(By.XPATH, "//p[@id='message']")
+    fact_text = text_element.text
+except NoSuchElementException:
+    print('Элемент не найден')
+except StaleElementReferenceException:
+    print('Сохраненная ссылка на элемент стала недействительной')
 print(f"\nФактический текст:\t'{fact_text}' \nОжидаемый текст:\t'{text_value}'", sep='')
 time.sleep(1)
 assert fact_text == text_value, 'Отображаемый в превью текст должен совпадать с введенным'
@@ -67,9 +87,15 @@ print("Превью текста отображается корректно")
 expected_sum = float(first_value + second_value)
 fact_sum = None  #  Если отображаемая в превью сумма не найдется
 try:
-    fact_sum = float(driver.find_element(By.XPATH,"//p[@id='addmessage']").text)
-except:
-    fact_sum = "Not_a_Number"  #  если отображается НЕчисло
+    sun_element = driver.find_element(By.XPATH,"//p[@id='addmessage']")
+    fact_sum = float(sun_element.text)
+except NoSuchElementException:
+    print('Элемент не найден')
+except ValueError:
+    fact_sum = "Not_a_Number"
+    print('Отображается НЕчисло')
+except StaleElementReferenceException:
+    print('Сохраненная ссылка на элемент стала недействительной')
 print(f"\nФактическая сумма:\t'{fact_sum}', \nОжидаемая сумма:\t'{expected_sum}'", sep='')
 time.sleep(1)
 assert fact_sum == expected_sum, 'Отображаемая сумма должна совпадать с вычисленной'
